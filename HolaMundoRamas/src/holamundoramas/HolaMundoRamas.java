892bf05276c4c79fdd058166626e27a3d52a8f54
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-01-10 13:48:44 +0100

    Inicio

---
+++ HolaMundoRamas/src/holamundoramas/HolaMundoRamas.java
@@ -0,0 +1,19 @@
+package holamundoramas;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class HolaMundoRamas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+        System.out.println("Fin de la cita");
+        
+    }
+    
+}
